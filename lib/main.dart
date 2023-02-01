0b63c516c1837383dc1615edb182a94b763fc96b
“RumitPatel” <rumit.patel5@gmail.com>
2023-02-02 00:29:20 +0530

    setState checked.

--- lib/main.dart
+++ lib/main.dart
@@ -55,6 +55,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  String randomName = '';
 
   void _incrementCounter() {
     setState(() {
@@ -64,6 +65,7 @@ class _MyHomePageState extends State<MyHomePage> {
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
       _counter++;
+      randomName = WordPair.random().toString();
     });
   }
 
@@ -123,7 +125,7 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             // Text(appState.current.asLowerCase),
             Text(
-              'Random words:\n${WordPair.random().asLowerCase}',
+              'Random words:\n$randomName',
               style: TextStyle(fontSize: 30),
             ),
           ],
