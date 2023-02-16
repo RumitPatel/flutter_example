0c2baea6a6f1f40cd77f9d53423fbd94ed080cda
RumitPatel <rumit.patel5@gmail.com>
2023-02-16 10:53:23 +0530

    MainListItem.dart POJO class added.

---
+++ lib/models/MainListItem.dart
@@ -0,0 +1,15 @@
+class MainListItem {
+  final String id;
+  final String name;
+
+  MainListItem(this.id, this.name);
+
+/*  MainListItem.fromJson(Map<String, dynamic> json)
+      : id = json['name'],
+        name = json['email'];
+
+  Map<String, dynamic> toJson() => {
+    'name': id,
+    'email': name,
+  };*/
+}
