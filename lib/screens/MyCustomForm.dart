95e6be34bf03ea1cf602b1db94d9cc7245210c0f
“RumitPatel” <rumit.patel5@gmail.com>
2023-02-18 19:29:14 +0530

    MyCustomForm created.

---
+++ lib/screens/MyCustomForm.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class MyCustomForm extends StatefulWidget {
+  const MyCustomForm({Key? key}) : super(key: key);
+
+  @override
+  State<MyCustomForm> createState() => _MyCustomFormState();
+}
+
+class _MyCustomFormState extends State<MyCustomForm> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
