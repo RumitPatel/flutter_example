0dfdb7d617a9fc340e48c11d1884f992a7ea9005
“RumitPatel” <rumit.patel5@gmail.com>
2023-02-18 19:20:55 +0530

    mainItem1Text changed.

--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -2,5 +2,5 @@ const double textSizeNormal = 18;
 const double inputRadios = 15;
 const double inputContentPadding = 20;
 
-const String mainItem1Text="List Item Two";
+const String mainItem1Text="Word Pair Test Page";
 const String mainItem2Text="List Item Two";
