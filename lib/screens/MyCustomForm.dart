7a641db75963815248a9337823776637b87d215a
“RumitPatel” <rumit.patel5@gmail.com>
2023-02-18 22:09:45 +0530

    rename app_utils.dart

--- lib/screens/MyCustomForm.dart
+++ lib/screens/MyCustomForm.dart
@@ -1,4 +1,4 @@
-import 'package:android_flutter_examle/utilities/apputils.dart';
+import 'package:android_flutter_examle/utilities/app_utils.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
