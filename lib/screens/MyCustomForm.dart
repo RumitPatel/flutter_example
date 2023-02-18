7ce5a16238bf733ba8450120141639f49ed3b638
“RumitPatel” <rumit.patel5@gmail.com>
2023-02-18 21:55:07 +0530

    TextField added.

--- lib/screens/MyCustomForm.dart
+++ lib/screens/MyCustomForm.dart
@@ -24,48 +24,47 @@ class MyCustomFormState extends State<MyCustomForm> {
   Widget build(BuildContext context) {
     // Build a Form widget using the _formKey created above.
 
-    return MaterialApp(
-      title: 'This is app title',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('This is app title'),
-        ),
-        body: Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              TextFormField(
-                // The validator receives the text that the user has entered.
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter some text';
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('This is app title'),
+      ),
+      body: Form(
+        key: _formKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TextFormField(
+              // The validator receives the text that the user has entered.
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter valid email address';
+                }
+                return null;
+              },
+              decoration: new InputDecoration(
+                  hintText: 'Enter your email address',
+                  contentPadding: EdgeInsets.only(
+                      left: 15, bottom: 11, top: 11, right: 15)),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  // Validate returns true if the form is valid, or false otherwise.
+                  if (_formKey.currentState!.validate()) {
+                    // If the form is valid, display a snackbar. In the real world,
+                    // you'd often call a server or save the information in a database.
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text('Processing Data')),
+                    );
                   }
-                  return null;
                 },
+                child: const Text('Submit'),
               ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 16.0),
-                child: ElevatedButton(
-                  onPressed: () {
-                    // Validate returns true if the form is valid, or false otherwise.
-                    if (_formKey.currentState!.validate()) {
-                      // If the form is valid, display a snackbar. In the real world,
-                      // you'd often call a server or save the information in a database.
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(content: Text('Processing Data')),
-                      );
-                    }
-                  },
-                  child: const Text('Submit'),
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
-
-
   }
 }
