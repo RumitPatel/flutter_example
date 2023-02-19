26c82bda25d05f8988b98df3f04eeeef603840d3
“RumitPatel” <rumit.patel5@gmail.com>
2023-02-19 18:41:18 +0530

    form children moved to center.

--- lib/screens/my_custom_form.dart
+++ lib/screens/my_custom_form.dart
@@ -21,54 +21,57 @@ class MyCustomFormState extends State<MyCustomForm> {
       appBar: AppBar(
         title: const Text(formTitle),
       ),
-      body: Form(
-        key: _formKey,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            TextFormField(
-              keyboardType: TextInputType.emailAddress,
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return msgIncorrectEmailError;
-                }
-                if (isValidEmail(value)) {
-                  return msgIncorrectEmailError;
-                }
-                return null;
-              },
-              decoration: const InputDecoration(
-                  hintText: lblEmailPlaceholder,
-                  contentPadding: EdgeInsets.only(
-                      left: 15, bottom: 11, top: 11, right: 15)),
-            ),
-            TextFormField(
-              obscureText: true,
-              enableSuggestions: false,
-              autocorrect: false,
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return msgIncorrectPasswordError;
-                }
-                return null;
-              },
-              decoration: const InputDecoration(
-                  hintText: lblPasswordPlaceholder,
-                  contentPadding: EdgeInsets.only(
-                      left: 15, bottom: 11, top: 11, right: 15)),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16.0),
-              child: ElevatedButton(
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    showSnackBar(context, tempTextProcessingData);
+      body: Center(
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextFormField(
+                keyboardType: TextInputType.emailAddress,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return msgIncorrectEmailError;
                   }
+                  if (isValidEmail(value)) {
+                    return msgIncorrectEmailError;
+                  }
+                  return null;
                 },
-                child: const Text(lblSubmit),
+                decoration: const InputDecoration(
+                    hintText: lblEmailPlaceholder,
+                    contentPadding: EdgeInsets.only(
+                        left: 15, bottom: 11, top: 11, right: 15)),
               ),
-            ),
-          ],
+              TextFormField(
+                obscureText: true,
+                enableSuggestions: false,
+                autocorrect: false,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return msgIncorrectPasswordError;
+                  }
+                  return null;
+                },
+                decoration: const InputDecoration(
+                    hintText: lblPasswordPlaceholder,
+                    contentPadding: EdgeInsets.only(
+                        left: 15, bottom: 11, top: 11, right: 15)),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      showSnackBar(context, tempTextProcessingData);
+                    }
+                  },
+                  child: const Text(lblSubmit),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
