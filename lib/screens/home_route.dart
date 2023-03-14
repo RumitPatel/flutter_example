d3b7aff6ae12b6ce5d46c99f4f6daa4e73728967
RumitPatel <rumit.patel5@gmail.com>
2023-03-14 10:22:22 +0530

    option renamed from mainItem4Text to mainItem3Text.

--- lib/screens/home_route.dart
+++ lib/screens/home_route.dart
@@ -11,7 +11,7 @@ class HomeRoute extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var mainItems = <String>[mainItem1Text, mainItem2Text, mainItem4Text];
+    var mainItems = <String>[mainItem1Text, mainItem2Text, mainItem3Text];
 
     return Scaffold(
       appBar: AppBar(
