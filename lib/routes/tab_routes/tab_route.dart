f74b5b8c8239c8293619f1c196d72878a0ea9d8f
“RumitPatel” <rumit.patel5@gmail.com>
2023-03-23 08:47:20 +0530

    Bottom items inserted.

--- lib/routes/tab_routes/tab_route.dart
+++ lib/routes/tab_routes/tab_route.dart
@@ -1,13 +1,24 @@
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class TabRoute extends StatelessWidget {
   const TabRoute({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Welcome to is tab route"),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Tab route title'),
+      ),
+      body: Center(),
+      bottomNavigationBar: BottomNavigationBar(items: [
+        BottomNavigationBarItem(label: "Item 1", icon: Icon(Icons.home)),
+        BottomNavigationBarItem(
+            label: "Item 2", icon: Icon(Icons.shopping_cart)),
+        BottomNavigationBarItem(
+            label: "Settings", icon: Icon(Icons.settings)),
+      ],),
     );
   }
 }
