940abf3b5707e609a3f8aaba381c3c84bf5a5896
RumitPatel <rumit.patel5@gmail.com>
2023-04-07 19:51:43 +0530

    TabHomeRoute created.

---
+++ lib/routes/tab_routes/tab_home_route.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+
+class TabHomeRoute extends StatefulWidget {
+
+  @override
+  State<TabHomeRoute> createState() => _TabHomeRoute();
+}
+class _TabHomeRoute extends State<TabHomeRoute> {
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      "This is Tab home route"
+    );
+  }
+
+}
