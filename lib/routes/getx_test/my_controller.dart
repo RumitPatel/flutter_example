f90995a2e326bb8659fc0b146e921a3a94cb96d9
RumitPatel <rumit.patel5@gmail.com>
2024-01-28 16:24:44 +0530

    getX example added in word_pair_home_route.dart

--- lib/routes/getx_test/my_controller.dart
+++ lib/routes/getx_test/my_controller.dart
@@ -1,14 +1,11 @@
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_example/routes/state_test/state_test.dart';
 import 'package:get/get.dart';
 
 import '../../utilities/app_utils.dart';
 
 class MyController extends GetxController {
-  var count = 0.obs;
-
-  increment() => {count.value = getRandomInt()};
-
   var current = WordPair.random().obs;
   var history = <WordPair>[].obs;
 
@@ -61,5 +58,47 @@ class MyController extends GetxController {
     } else {
       return Icons.favorite_border;
     }
+  }
+
+  var tasks = <Task>[
+    Task(id: 1, label: "Load rocket with supplies"),
+    Task(id: 2, label: "Launch rocket"),
+    Task(id: 3, label: "Circle the home planet"),
+    Task(id: 4, label: "Head out to the first moon"),
+    Task(id: 5, label: "Launch moon lander #1"),
+  ].obs;
+
+/*  void add(Task task) {
+    state = [...state, task];
+  }*/
+
+  void toggleTaskChecked(int taskId) {
+    printI('before toggle');
+    printTasks();
+
+    for (var i = 0; i < tasks.length; i++) {
+      var item = tasks[i];
+      if (taskId == item.id) {
+        tasks[i] = item.copyWith(completed: !item.completed);
+      }
+    }
+
+    printI('after toggle');
+    printTasks();
+  }
+
+  printTasks() {
+    for (final item in tasks) {
+      printI(
+          'id:${item.id}, label:${item.label}, completed:${item.completed},');
+    }
+  }
+
+  getCheckedTasks() {
+    var numCompletedTasks = tasks.where((task) {
+      return task.completed == true;
+    }).length;
+
+    return numCompletedTasks;
   }
 }
