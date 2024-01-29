bed85e50e4ed128dddb8235cb475d3fc964ab69a
RumitPatel <rumit.patel5@gmail.com>
2024-01-29 19:40:34 +0530

    Pojo class created.

---
+++ lib/routes/api_call_using_http_library/model/user.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+User _$UserFromJson(Map<String, dynamic> json) => User(
+      json['name'] as String,
+      json['email'] as String,
+      Address.fromJson(json['address'] as Map<String, dynamic>),
+    );
+
+Map<String, dynamic> _$UserToJson(User instance) => <String, dynamic>{
+      'name': instance.name,
+      'email': instance.email,
+      'address': instance.address.toJson(),
+    };
